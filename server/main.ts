3aff2879eda4bb6730962dc3fdc7a777e890e29e
aejeong <aejeong-context@kakao.com>
2024-03-03 17:11:13 +0900

    init: create next-next-project

---
+++ server/main.ts
@@ -0,0 +1,30 @@
+import { INestApplication } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as http from "http";
+import { NextApiHandler } from "next";
+
+
+export module Main{
+    let app: INestApplication;
+
+    export async function getApp() {
+        if(!app){
+            app = await NestFactory.create(AppModule,{bodyParser:false});
+            app.setGlobalPrefix("api");
+
+            await app.init();
+        }
+
+        return app;
+    }
+
+    export async function getListener() {
+        const app = await getApp();
+        const server: http.Server = app.getHttpServer();
+        const [listener] = server.listeners("request") as NextApiHandler[];
+    
+        return listener;
+      }
+
+}
